4076322148b4353f3c9fa8a4ae32367ad9619b44
ManuCarSeg24 <manucarseg@gmail.com>
2026-02-18 13:39:27 +0100

    Correccion de la coleccion postman, mas eliminacion de la tabla pedidos del proyecto

--- src/main/java/es/iesjandula/SampleMongoServer/repository/IClienteRepository.java
+++ src/main/java/es/iesjandula/SampleMongoServer/repository/IClienteRepository.java
@@ -1,7 +1,5 @@
 package es.iesjandula.SampleMongoServer.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +8,5 @@ import es.iesjandula.SampleMongoServer.model.Cliente;
 @Repository
 public interface IClienteRepository extends MongoRepository<Cliente, String> 
 {
-    Optional<Cliente> findByEmail(String email);
-
-    boolean existsByEmail(String email);
+	
 }
